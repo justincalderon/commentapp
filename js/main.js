7e4e66aaa141ca1146448968c5a16fe4ecbf1e62
justincalderon <justin.calderon@outlook.com>
2017-03-23 00:10:02 -0700

    Comment App!

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function commentArea() {
+  var username = document.getElementById("username").value
+  var comment = document.getElementById("comment").value
+  console.log(username + " " + comment);
+}
